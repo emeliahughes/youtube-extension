ba7121e288bdd2d7f7039020a9fdc2641dd21818
Renee <renee17@uw.edu>
2021-10-20 22:36:33 -0700

    react interface posts to database

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 'use strict';
 import React from 'react';
 import AddNewCitation from './newcitation';
-import Yite from './Yite';
 
 const baseUrl = "https://youtubeextdata.azurewebsites.net/";
 const postUrl = baseUrl + "insertVideoData";
@@ -13,35 +12,23 @@ function App(){
     // Each YouTube video has a unique ID, videoID.
     const videoID = document.querySelector("#watch7-content > meta:nth-child(6)").content;
 
-    console.log(videoID);
+//    console.log(videoID);
 
     // We make a GET call to the getUrl to get the citation data for this particular video.
     let responseData = getData(videoID, getUrl);
 
     // If the video doesn't exist in the database, we make a new map.
     // videoCitations is organized by start time, such that key: starttime(int) -> value: citations(array of citations)
-    let videoCitations = new Map();
     if (responseData.length > 0) {
-        videoCitations = JSON.parse(responseData);
+        console.log(videoCitations);
     }
-
-    // ********* Creating a new citation ******************
-    let newTestCitation = new Yite("0:05", "0:15", "hello", "world", "harry potter", "www.youtube.com");
-    if(!videoCitations.has(newTestCitation.startTime)) {
-        videoCitations.set(newTestCitation.startTime, []);
-    }
-    videoCitations.get(newTestCitation.startTime).push(newTestCitation)
-    // Push updates to database.
-    // TODO: Once there is a more well-defined insertion process, we can change this so it pushes every couple (maybe 5?) citations.
-    pushData(videoCitations, videoID);
-    // ******************************************************
 
     return (
         <div className="citation-box">
-            <div class="header">
+            <div className="header">
                 <h1>"Citations"</h1>
             </div>
-            <div class="citation-viewer">
+            <div className="citation-viewer">
                 {/* add slider here and citation viewer */}
                 <AddNewCitation />
             </div>
@@ -63,24 +50,6 @@ function getData(videoID) {
     .then(result => {data = result;})
     .catch(err => console.log(err));
     return data;
-}
-
-/**
- * Makes a POST request to update data for a particular video.
- * @param {*} citations citations to be inserted
- * @param {*} videoID unique ID of the video
- */
-function pushData(citations, videoID) {
-    let insertCitation = {"id": videoID, "citations": citations};
-    let promise = fetch(postUrl, {
-        method: "POST",
-        body: JSON.stringify(insertCitation),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-    promise
-    .catch(err => console.log(err));
 }
 
 export default App;
--- src/newcitation.js
+++ src/newcitation.js
@@ -1,6 +1,17 @@
 import React, {useState } from 'react';
+import Yite from './Yite';
 
 //TODO: error handling for times out of bounds, update to add citation to database
+const baseUrl = "https://youtubeextdata.azurewebsites.net/";
+const postUrl = baseUrl + "insertVideoData";
+const getUrl = baseUrl + "getTimeStamps?id=";
+
+const videoID = document.querySelector("#watch7-content > meta:nth-child(6)").content;
+let responseData = getData(videoID, getUrl);
+let videoCitations = new Map();
+if (responseData.length > 0) {
+    videoCitations = JSON.parse(responseData);
+}
 
 function AddNewCitation (props) {
 
@@ -44,12 +55,12 @@ function AddNewCitation (props) {
     const handleSubmit = (event) => {
         event.preventDefault();
 
-        let newCitation = {
-            title: inputTitleValue,
-            source: inputSourceValue,
-            startTime: inputStartTimeValue,
-            endTime: inputEndTimeValue,
+        let newYite = new Yite(inputStartTimeValue, inputEndTimeValue, inputTitleValue, inputSourceValue, "author", "link");
+        if(!videoCitations.has(newYite.startTime)) {
+            videoCitations.set(newYite.startTime, []);
         }
+        videoCitations.get(newYite.startTime).push(newYite);
+        pushData(videoCitations, videoID);
         
         //ADD NEW CITATION TO DATABASE HERE
         //this is how I did it in firebase, it should be pretty similar
@@ -70,21 +81,21 @@ function AddNewCitation (props) {
                 <div className="form-around">
                     <h2>Add a new citation:</h2>
                     <div>
-                        <label for="title_field" className="main-labels"><h3 className="small">Source Title:</h3></label>
+                        <label htmlFor="title_field" className="main-labels"><h3 className="small">Source Title:</h3></label>
                         <input type="text" 
                             onChange={handleTitle} 
                             value={inputTitleValue} 
                             className="form-control" id="title_field" name="title" required/>
                     </div>
                     <div>
-                        <label for="source_field" className="main-labels"><h3 className="small">Source:</h3></label>
+                        <label htmlFor="source_field" className="main-labels"><h3 className="small">Source:</h3></label>
                         <input type="text" 
                             onChange={handleSource} 
                             value={inputSourceValue} 
                             className="form-control" id="source_field" name="source" required/>
                     </div>
                     <div>
-                        <label for="link_field" className="main-labels"><h3 className="small">Link:</h3></label>
+                        <label htmlFor="link_field" className="main-labels"><h3 className="small">Link:</h3></label>
                         <input type="text" 
                             onChange={handleLink} 
                             value={inputLinkValue} 
@@ -93,15 +104,15 @@ function AddNewCitation (props) {
                     <div className="row">
                         <div>
                             <div>
-                                <label for="start_time_field" className="main-labels"><h3 className="small">From: </h3></label>
-                                <input type="time" 
+                                <label htmlFor="start_time_field" className="main-labels"><h3 className="small">From: </h3></label>
+                                <input type="text" 
                                     onChange={handleStartTime} 
                                     value={inputStartTimeValue} 
                                     className="form-control" id="start_time_field" name="start_time" required/>
                             </div>
                             <div>
-                                <label for="end_time_field" className="main-labels"><h3 className="small"> to </h3></label>
-                                <input type="time" 
+                                <label htmlFor="end_time_field" className="main-labels"><h3 className="small"> to </h3></label>
+                                <input type="text" 
                                     onChange={handleEndTime} 
                                     value={inputEndTimeValue} 
                                     className="form-control" id="end_time_field" name="end_time" required/>
@@ -115,6 +126,42 @@ function AddNewCitation (props) {
             </form>
         </div>
     )
+}
+
+/**
+ * Makes a GET request to get the data for a particular video.
+ * @param {string} videoID unique ID of the video
+ * @returns all data for a video
+ */
+ function getData(videoID) {
+    let data = [];
+    let requestUrl = getUrl + videoID;
+    let promise = fetch(requestUrl);
+    promise
+    .then((resp) => (resp.json()))
+    .then(result => {data = result;})
+    .catch(err => console.log(err));
+    return data;
+}
+
+/**
+ * Makes a POST request to update data for a particular video.
+ * @param {*} citations citations to be inserted
+ * @param {*} videoID unique ID of the video
+ */
+function pushData(citations, videoID) {
+    let insertCitation = {"id": videoID, "citations": JSON.stringify(Object.fromEntries(citations))};
+    console.log(insertCitation);
+    console.log(JSON.stringify(insertCitation));
+    let promise = fetch(postUrl, {
+        method: "POST",
+        body: JSON.stringify(insertCitation),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    promise
+    .catch(err => console.log(err));
 }
 
 export default AddNewCitation;
