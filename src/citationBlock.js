cbeff929eec55fd61d182373490c3c9ac4028233
Anson Huang <ans.hua883@gmail.com>
2023-01-20 01:30:22 -0800

    click tracking working

--- src/citationBlock.js
+++ src/citationBlock.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import convertTimeToSeconds from './timeToSeconds';
+import trackUserClick from './trackUserClick';
+import { USER_CLICK } from './constants/clicks';
 
 function Citation(props) {
     let citation = props.citation;
@@ -42,11 +44,11 @@ function Citation(props) {
                 <div className='col p-2 mr-auto ml-4'>
                     <h4 className="citation-source card-title font-weight-bold pb-1">{source}</h4>
                     <h4 className="card-text pb-1">{description}</h4>
-                    <a href={link} className="citation-source card-link pb-1" target="_blank"><h4>Visit {sourceType}</h4></a>
+                    <a href={link} onClick={trackUserClick.bind(this, USER_CLICK.ARTICLE_LINK)} className="citation-source card-link pb-1" target="_blank"><h4>Visit {sourceType}</h4></a>
                 </div>
                 <div className='col-auto m-2 mr-auto'>
                     <div className='row justify-content-end m-3'>
-                        <button onClick={jumpTime.bind(this, startTime)} className='btn btn-secondary rounded-lg card-link align-items-center p-2 mx-2 mr-0 ml-2'>
+                        <button onClick={jumpTime.bind(this, startTime, true)} className='btn btn-secondary rounded-lg card-link align-items-center p-2 mx-2 mr-0 ml-2'>
                             <h4 className='mb-0 p-1'>{citation.startTime}</h4>
                         </button>
                         <h4 className='card-text text-italic align-self-center mb-0 m-2'>to</h4>
@@ -60,7 +62,10 @@ function Citation(props) {
     )
 }
 
-function jumpTime(time) {
+function jumpTime(time, isStartTime=false) {
+    if (isStartTime)
+        trackUserClick(USER_CLICK.START_TIME);
+
     let video = document.getElementsByTagName("video")[0];
     video.currentTime = time;
 }
---
+++ src/constants/clicks.js
@@ -0,0 +1,6 @@
+export const USER_CLICK = {
+    ARTICLE_LINK: "articleLink",
+    LIST_VIEW_CITATION: "listViewCitation",
+    START_TIME: "startTime",
+    TIMELINE_VIEW_CITATION: "timelineViewCitation"
+}
